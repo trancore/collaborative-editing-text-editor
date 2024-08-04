57d74fa8093e3cccaec3a7bbb1daa10a5f475c13
trancore <ki.program0915@gmail.com>
2024-08-04 16:14:27 +0900

    first push

---
+++ src/app/components/CrdtComponent.tsx
@@ -0,0 +1,74 @@
+﻿"use client";
+
+import { CollaborationPlugin } from "@lexical/react/LexicalCollaborationPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import {
+  $createParagraphNode,
+  $createTextNode,
+  $getRoot,
+  LexicalEditor,
+} from "lexical";
+import { WebsocketProvider } from "y-websocket";
+
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+import * as Y from "yjs";
+
+function initialEditorState(editor: LexicalEditor): void {
+  const root = $getRoot();
+  const paragraph = $createParagraphNode();
+  const text = $createTextNode("Welcome to collab!");
+  paragraph.append(text);
+  root.append(paragraph);
+}
+
+function CollaborativeEditor() {
+  const initialConfig = {
+    // NOTE: This is critical for collaboration plugin to set editor state to null. It
+    // would indicate that the editor should not try to set any default state
+    // (not even empty one), and let collaboration plugin do it instead
+    editorState: null,
+    namespace: "Demo",
+    nodes: [],
+    onError: (error: Error) => {
+      throw error;
+    },
+    theme: {},
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable id="textform" />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <CollaborationPlugin
+        id="yjs-plugin"
+        // @ts-ignore
+        providerFactory={(id, yjsDocMap) => {
+          const doc = new Y.Doc();
+          yjsDocMap.set(id, doc);
+
+          const provider = new WebsocketProvider(
+            "ws://localhost:1234",
+            id,
+            doc
+          );
+
+          return provider;
+        }}
+        // Optional initial editor state in case collaborative Y.Doc won't
+        // have any existing data on server. Then it'll user this value to populate editor.
+        // It accepts same type of values as LexicalComposer editorState
+        // prop (json string, state object, or a function)
+        initialEditorState={initialEditorState}
+        shouldBootstrap={true}
+      />
+    </LexicalComposer>
+  );
+}
+
+export default CollaborativeEditor;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import CrdtComponent from "~/app/components/CrdtComponent";
+
+export default function Home() {
+  return (
+    <main>
+      <CrdtComponent></CrdtComponent>
+    </main>
+  );
+}
